4ad5bf06ddfcce672d14783f80d515f23be86f2c
Tayyab_rajput <102616744+muhammadtayyabrajput@users.noreply.github.com>
2025-01-26 14:42:24 +0500

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,7 +11,7 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  const componentMounted = useRef(true);
+  let componentMounted = true;
 
   const dispatch = useDispatch();
 
